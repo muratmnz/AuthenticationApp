d52da5b5637da2e0e2c31099f000150844d945c9
muratmnz <minazzmurat@gmail.com>
2023-01-14 00:51:45 +0300

    First commit

---
+++ app/src/main/java/com/muratmnz/loginregisterapp/Login/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.muratmnz.loginregisterapp.Login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.muratmnz.loginregisterapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
